cc4fc5624a4c1adf4123507e685cdf029ba0218b
yanginto1996 <yanginto@gmail.com>
2024-10-05 13:50:16 -0700

    working button click on display

--- NUMAD24FA_YanTo/app/src/main/java/com/example/hello_world/QuicCalcActivity.java
+++ NUMAD24FA_YanTo/app/src/main/java/com/example/hello_world/QuicCalcActivity.java
@@ -1,6 +1,9 @@
 package com.example.hello_world;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class QuicCalcActivity extends AppCompatActivity {
+
+    private TextView calcDisplay;
+    private String curr;
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnadd, btnminus, btnequals, btndel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,125 @@ public class QuicCalcActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        calcDisplay = findViewById(R.id.calc_display);
+        curr = "";
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnadd = findViewById(R.id.btnadd);
+        btnminus = findViewById(R.id.btnsubtract);
+        btndel = findViewById(R.id.btnx);
+        btnequals = findViewById(R.id.btnequals);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "0";
+                display();
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "1";
+                display();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "2";
+                display();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "3";
+                display();
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "4";
+                display();
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "5";
+                display();
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "6";
+                display();
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "7";
+                display();
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "8";
+                display();
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "9";
+                display();
+            }
+        });
+
+        btndel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delete();
+                display();
+            }
+        });
+
+
     }
+
+    public void display() {
+        calcDisplay.setText(curr);
+    }
+
+    public void delete() {
+        if (!curr.isEmpty()) {
+            curr = curr.substring(0, curr.length() - 1);
+        }
+    }
+
+
 }
+
