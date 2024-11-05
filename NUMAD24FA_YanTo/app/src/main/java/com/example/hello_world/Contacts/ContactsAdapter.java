746e7d28b5ff82a1fca83e6ecb95c47ebd1d4f09
yanto1996 <to.y@northeastern.edu>
2024-11-04 21:06:51 -0800

    fixed horizontal constraints on main activity

--- NUMAD24FA_YanTo/app/src/main/java/com/example/hello_world/Contacts/ContactsAdapter.java
+++ NUMAD24FA_YanTo/app/src/main/java/com/example/hello_world/Contacts/ContactsAdapter.java
@@ -26,6 +26,7 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsViewHolder> {
         this.list = list;
         this.contactLauncher = contactLauncher;
     }
+    
     @NonNull
     @Override
     public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
